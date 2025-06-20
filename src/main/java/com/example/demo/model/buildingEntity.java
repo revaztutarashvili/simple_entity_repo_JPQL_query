3f18b8e2c78a0d7113a086a3075f9e0de44dede4
“Revaz <generationofjeans@gmail.com>
2025-06-20 19:43:59 +0400

    add some new query

--- src/main/java/com/example/demo/model/buildingEntity.java
+++ src/main/java/com/example/demo/model/buildingEntity.java
@@ -1,20 +1,24 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class building {
+@Table(name = "buildingEntity")
+public class buildingEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID")
     private long id;
 
+    @Column()
     private int floor;
+    @Column()
     private boolean hasParking;
+    @Column()
     private String address;
+    @Column()
     private int entrance;
+    @Column()
     private int capacity;
 
 
---
+++ src/main/java/com/example/demo/model/humanEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "humanEntity")
+public class humanEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(unique = true)
+    private long identityNum;
+
+    @Column(name = "personName")
+    private String name;
+
+    @Column(name = "Sex")
+    private String sex;
+
+    @Column(name = "personAge")
+    private int age;
+
+    @Column(name = "weight")
+    private long weight;
+
+    @Column(name = "address")
+    private String address;
+}
