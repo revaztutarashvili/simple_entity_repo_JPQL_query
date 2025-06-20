826c1778aa12d4fcb19667f37f1f27b861ff8732
“Revaz <generationofjeans@gmail.com>
2025-06-20 19:48:15 +0400

    add new

--- src/main/java/com/example/demo/repository/humanRepository.java
+++ src/main/java/com/example/demo/repository/humanRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.building;
+import com.example.demo.model.buildingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface humanRepository extends JpaRepository<building,Long> {
+public interface humanRepository extends JpaRepository<humanRepository,Long> {
 
 }
